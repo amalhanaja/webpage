132826a1a9a59f711a74b47a8760209c53e59f5f
amalhanaja <amalhanaja@gmail.com>
2023-01-07 10:05:10 +0700

    [FEAT]: Update bio

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -7,13 +7,12 @@ export const Hero: FC = () => {
   const router = useRouter();
   return (
     <section className="relative flex flex-col p-4 mt-4">
-      <h2 className="text-primary text-2xl font-bold">
-        Hello world
-      </h2>
+      <h2 className="text-primary text-2xl font-bold">Hello world</h2>
       <p className="mt-2 text-primary-text">
-        I&apos;m Alfian, a software engineer Sidoarjo, Indonesia 🇮🇩 with
-        rock-solid experience in building complex applications with modern
-        technologies.
+        I&apos;m Alfian Akmal Hanantio. I&apos;m a software engineer specialized
+        in android development based in Sidoarjo, Indonesia 🇮🇩 with rock-solid
+        experience in building complex applications with modern technologies.
+        I&apos;m currently learning everything.
       </p>
     </section>
   );
