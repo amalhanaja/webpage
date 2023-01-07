d020474242942f8b9a879c0f131095555b5bb0a4
amalhanaja <amalhanaja@gmail.com>
2023-01-07 10:51:48 +0700

    [FEAT]: update project item html tag

--- components/Projects/ProjectItem.tsx
+++ components/Projects/ProjectItem.tsx
@@ -22,7 +22,7 @@ export const ProjectItem: FC<ProjectItemProps> = ({ project }) => (
   <a href={project.projectUrl} target="__blank" className="hover:bg-background-dark hover:rounded-lg">
     <div className="p-2 flex flex-col">
       <h4 className="font-semibold text-lg">{project.title}</h4>
-      <div className="mt-1 text-content-200-dark">{project.description}</div>
+      <p className="mt-1 text-content-200-dark">{project.description}</p>
       <div className="pb-4 mt-2 flex flex-wrap">
         {project.tags.map((tag, i) => (
           <ProjectTag key={tag} tag={tag} />
