99ae43ff348ca35101b9942b7d9f246a782f56d9
amalhanaja <amalhanaja@gmail.com>
2023-08-20 01:02:01 +0700

    feat: add icons

---
+++ src/lib/components/Icons/ArrowRight.svelte
@@ -0,0 +1,16 @@
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M4.91381 11.645H18.9138"
+		stroke="current"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M11.9138 4.64499L18.9138 11.645L11.9138 18.645"
+		stroke="current"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
