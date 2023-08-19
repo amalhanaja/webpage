866efd325492438f02ca06687b2b8d99beff9428
amalhanaja <amalhanaja@gmail.com>
2023-08-20 01:41:02 +0700

    feat: remove icon width and height

--- src/lib/components/Icons/Kotlin.svelte
+++ src/lib/components/Icons/Kotlin.svelte
@@ -1,6 +1,6 @@
-<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path
 		d="M29.44 30.3585H3.84L16.64 17.5585L29.44 30.3585ZM28.8 3.47852L2.56 29.8273V17.4497L16.5312 3.47852H28.8ZM14.72 3.47852L2.56 15.6385V3.47852H14.72Z"
-		fill="current"
+		fill="currentColor"
 	/>
 </svg>
