1e4b554faa85a1bfe8d177a2bd6d01897bf52925
amalhanaja <amalhanaja@gmail.com>
2023-08-20 00:37:56 +0700

    feat: add header

---
+++ src/lib/components/AppBar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Logo from './Icons/Logo.svelte';
+</script>
+
+<header class="p-4">
+	<nav class="bg-surface flex w-full justify-center rounded-lg py-3"><Logo /></nav>
+</header>
