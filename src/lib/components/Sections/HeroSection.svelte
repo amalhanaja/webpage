fe9c0d514d7f9dcd04ec7d5655dba72ad9495dd1
amalhanaja <amalhanaja@gmail.com>
2023-08-21 04:17:10 +0700

    feat: improve hero html structure

--- src/lib/components/Sections/HeroSection.svelte
+++ src/lib/components/Sections/HeroSection.svelte
@@ -11,18 +11,20 @@
 		I'm <span class="text-primary">Alfian</span> <span class="text-secondary">Hanantio</span>
 	</h1>
 	<h2 class="text-2xl font-medium">Software Engineer</h2>
-	<p class="mt-4 text-sm">
-		I love writing code that takes things to the next level by creating highly performant
-		applications, automated API integrations, building my own dev-tools, and creating stunning user
-		experiences that make you feel WOW!
-	</p>
 
-	<p class="mt-5 text-sm">
-		I am always keen to learn and explore new technologies, frameworks, and programming languages.
-		Currently, I am exploring SvelteKit and Rust.
-	</p>
+	<article class="my-5 text-sm">
+		<p>
+			I love writing code that takes things to the next level by creating highly performant
+			applications, automated API integrations, building my own dev-tools, and creating stunning
+			user experiences that make you feel WOW!
+		</p>
+		<p class="mt-5">
+			I am always keen to learn and explore new technologies, frameworks, and programming languages.
+			Currently, I am exploring SvelteKit and Rust.
+		</p>
+	</article>
 
-	<div class="mt-5 flex gap-2">
+	<div class="flex gap-2">
 		{#each socials as s}
 			<SocialMedia href={s.url} key={s.key}></SocialMedia>
 		{/each}
