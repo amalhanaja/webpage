8a588279ed438cb3802d9f2fdcc842cb506c8e8c
amalhanaja <amalhanaja@gmail.com>
2023-08-22 01:32:14 +0700

    feat: Section Header component

---
+++ src/lib/components/SectionHeader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import ArrowRight from './Icons/ArrowRight.svelte';
+
+	export let title: string;
+	export let href: string = '';
+</script>
+
+<section class="flex justify-between">
+	<div class="rotate-2">
+		<h2 class="text-4xl font-bold text-primary">{title}</h2>
+		<div class="h-1 w-28 rounded-sm bg-secondary"></div>
+	</div>
+	{#if href}
+		<a {href} class="flex items-center justify-center text-base font-semibold text-primary gap-1"
+			>See More<span class="h-6 w-6"><ArrowRight /></span></a
+		>
+	{/if}
+</section>
