18355b0181064c80792507520ae8355aeb1be7c9
amalhanaja <amalhanaja@gmail.com>
2023-08-23 00:12:14 +0700

    feat: featured article

---
+++ src/lib/components/Sections/FeaturedArticleSection.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import FeaturedArticle from '../FeaturedArticle.svelte';
+	import SectionHeader from '../SectionHeader.svelte';
+</script>
+
+<section class="py-8">
+	<SectionHeader title="Articles" />
+	<h3 class="mt-4 font-medium">I love to share my knowledge through writing.</h3>
+	<FeaturedArticle />
+</section>
