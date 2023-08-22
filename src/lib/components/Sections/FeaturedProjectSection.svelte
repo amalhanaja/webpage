64faf65eecb33df7058b27e861b781c4820bedb9
amalhanaja <amalhanaja@gmail.com>
2023-08-22 20:47:19 +0700

    feat: featured projects

--- src/lib/components/Sections/FeaturedProjectSection.svelte
+++ src/lib/components/Sections/FeaturedProjectSection.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
+	import type { FeaturedProjectModel } from '$lib/models/ProjectModel';
 	import FeaturedProject from '../FeaturedProject.svelte';
 	import SectionHeader from '../SectionHeader.svelte';
+
+	export let projects: FeaturedProjectModel[];
 </script>
 
 <section class="py-8">
 	<SectionHeader title="Projects" />
 	<h3 class="mt-4 font-medium">A glimpse into my work</h3>
-	<FeaturedProject
-		project={{
-			title: 'DyahAcademy',
-			shortDescription: 'E-learning Application, Quizzes, and asdf',
-			imgSrc: 'https://placehold.co/400',
-			tags: ['Android', 'Typescript', 'PostgreSQL']
-		}}
-	/>
+	<section class="mt-6 grid grid-cols-1 gap-4">
+		{#each projects as project}
+			<FeaturedProject {project} />
+		{/each}
+	</section>
 </section>
