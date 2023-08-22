279cb988d9cfcb1c06c7b0acfe2e665b4e12e98d
amalhanaja <amalhanaja@gmail.com>
2023-08-23 04:22:02 +0700

    feat: featured article

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,13 +3,15 @@ import featuredProjects from '$lib/data/json/featured-projects.json';
 import type { FeaturedProjectModel } from '$lib/models/ProjectModel';
 import type { SocialMediaInfo } from '$lib/models/SocialMediaModel';
 import type { PageServerLoad } from './$types';
-import { fetchPosts } from '$lib/data/json/posts';
+import { fetchPosts, getPublicationUrl } from '$lib/data/json/posts';
 
 export const load: PageServerLoad = async () => {
 	const posts = await fetchPosts();
+	const publicationUrl = await getPublicationUrl();
 	return {
 		socials: <SocialMediaInfo[]>socialsMedia,
 		projects: <FeaturedProjectModel[]>featuredProjects,
-		posts
+		posts,
+		allPostUrl: publicationUrl
 	};
 };
