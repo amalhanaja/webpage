0e4ae54ffca205ba0f5814c0cfed10e140a7d509
amalhanaja <amalhanaja@gmail.com>
2023-08-26 12:05:44 +0700

    feat: add robots txt and sitemap

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -8,7 +8,8 @@ import { fetchPosts, getPublicationUrl } from '$lib/data/json/posts';
 export const load: PageServerLoad = async () => {
 	const posts = await fetchPosts();
 	const publicationUrl = await getPublicationUrl();
-	const siteUrl: string = process.env.SITE_URL ?? '';
+	const vercelUrl: string = process.env.VERCEL_URL ?? '';
+	const siteUrl = `https://${vercelUrl}`;
 	return {
 		socials: <SocialMediaInfo[]>socialsMedia,
 		projects: <FeaturedProjectModel[]>featuredProjects,
