1f6a34c3006387a700334723a9d0cfab4fa9158e
amalhanaja <amalhanaja@gmail.com>
2023-08-26 11:26:26 +0700

    feat: add meta seo tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MetaTags from '$lib/components/MetaTags.svelte';
 	import FeaturedArticleSection from '$lib/components/Sections/FeaturedArticleSection.svelte';
 	import FeaturedProjectSection from '$lib/components/Sections/FeaturedProjectSection.svelte';
 	import HeroSection from '$lib/components/Sections/HeroSection.svelte';
@@ -7,6 +8,14 @@
 	export let data: PageData;
 </script>
 
+<MetaTags
+	content={{
+		title: 'Alfian Akmal Hanantio - Software Engineer',
+		description: `I'm Alfian Akmal Hanantio. a software engineer specializing in android development based in Sidoarjo, Indonesia 🇮🇩`,
+		url: data.url,
+		siteUrl: data.siteUrl
+	}}
+/>
 <HeroSection socials={data.socials} />
 <WhatIDoSection />
 <FeaturedProjectSection projects={data.projects} />
