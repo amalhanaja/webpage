4d68b1181e7c43bc79906fe074e86436d879884b
amalhanaja <amalhanaja@gmail.com>
2024-06-10 08:31:03 +0700

    feat: data driven hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			size: {
+				120: '28rem'
+			}
+		}
+	},
+	plugins: []
+};
