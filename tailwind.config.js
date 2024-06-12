518a8050483d0cb2d7100a8bc4cc24346c543ba9
amalhanaja <amalhanaja@gmail.com>
2024-06-13 04:46:55 +0700

    feat: adjust tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
 		extend: {
 			size: {
 				120: '28rem'
+			},
+			boxShadow: {
+				xs: '1px 1px 0 0',
+				sm: '2px 2px 0 0',
+				md: '4px 4px 0 0',
+				lg: '6px 6px 0 0',
+				xl: '8px 8px 0 0'
+			},
+			borderWidth: {
+				1: '1px'
 			}
 		}
 	},
