217983987c2e4d1be1a1732d1d761361b94895c8
amalhanaja <amalhanaja@gmail.com>
2024-06-15 18:42:39 +0700

    feat: project external link

--- src/lib/components/home/projects.svelte
+++ src/lib/components/home/projects.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { Project, Skill } from '$lib/model';
 	import github from '$lib/assets/images/icons/github.svg';
+	import externalLink from '$lib/assets/images/icons/external-link.svg';
+	import { getRandomNumberInRange } from '$lib/helpers/randomizer';
+	import clsx from 'clsx';
 
 	export let projects: Project[];
 	export let skills: Record<string, Skill>;
@@ -8,14 +11,34 @@
 	const getProjectSkills = (project: Project): Skill[] => {
 		return project.skills.map((tag) => skills[tag]);
 	};
+
+	const getRandomColor = (): string => {
+		const colors = [
+			'bg-green-600',
+			'bg-red-600',
+			'bg-amber-600',
+			'bg-blue-600',
+			'bg-violet-600',
+			'bg-stone-600',
+			'bg-rose-600',
+			'bg-sky-600',
+			'bg-lime-600'
+		];
+		const index = getRandomNumberInRange(0, colors.length - 1);
+		return colors[index];
+	};
 </script>
 
 {#snippet skillItem(skill: Skill)}
-	<span class="border-black border-2 px-2 py-2">{skill.name}</span>
+	<span class={clsx('border-black border-2 px-2 py-1 shadow-sm text-white', getRandomColor())}
+		>{skill.name}</span
+	>
 {/snippet}
 
 {#snippet projectItem(project: Project)}
-	<div class="grid grid-cols-6 border-black border-4 shadow-xl hover:shadow-3xl transition-all duration-200">
+	<div
+		class="grid grid-cols-6 border-black border-4 shadow-xl hover:shadow-3xl transition-all duration-200"
+	>
 		<div class="col-span-4 border-r-2 border-black">
 			<img class="object-cover h-96 w-full" src={project.imageUrl} alt={project.name} />
 			<div class="px-6 h-20 align-middle border-t-4 border-black flex items-center">
@@ -23,26 +46,38 @@
 			</div>
 		</div>
 		<div class="text-base col-span-2 border-l-2 border-black flex flex-col">
-			<p class="text-base p-4 flex-1">{project.description}</p>
-			<div class="flex flex-row gap-2 flex-wrap px-4 py-4 border-t-4 border-black uppercase font-semibold">
+			<p class="text-base p-4 flex-1 font-light">{project.description}</p>
+			<div
+				class="flex flex-row gap-2 flex-wrap px-4 py-4 border-t-4 border-black uppercase font-semibold"
+			>
 				{#each getProjectSkills(project) as skill}
 					{@render skillItem(skill)}
 				{/each}
 			</div>
-			<div class="flex flex-row px-4 h-20 items-center gap-4 border-t-4 border-black bg-red-300">
-				<a
-					href={project.repoUrl}
-					class="btn-neobrutalism flex-1 flex py-2 px-3 gap-2 justify-center items-center"
-				>
-					<img src={github} alt="Github" class="size-8" />
-					<span>Github</span>
-				</a>
+			<div class="flex flex-row px-4 h-20 items-center gap-4 border-t-4 border-black bg-red-300 justify-end">
+				{#if project.repoUrl}
+					<a
+						href={project.repoUrl}
+						class="btn-neobrutalism flex-1 flex py-2 px-3 gap-2 justify-center items-center"
+					>
+						<img src={github} alt="Github" class="size-8" />
+						<span>Github</span>
+					</a>
+				{/if}
+				{#if project.link}
+					<a
+						href={project.link}
+						class="btn-neobrutalism flex py-3 px-3 gap-2 justify-center items-center"
+					>
+						<img src={externalLink} alt="Github" class="size-5" />
+					</a>
+				{/if}
 			</div>
 		</div>
 	</div>
 {/snippet}
 
-<section class="py-24">
+<section class="py-24 overflow-hidden">
 	<div class="max-w-screen-xl mx-auto">
 		<h2 class="text-5xl font-black mb-8 italic">Featured Projects</h2>
 		<div class="flex flex-col gap-6">
