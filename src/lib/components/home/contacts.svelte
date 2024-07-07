2d3f01fa4aef8cf82062be027809d651a8d7e0c3
amalhanaja <amalhanaja@gmail.com>
2024-07-07 23:02:05 +0700

    feat: fix hover animation due to conflix with aos

--- src/lib/components/home/contacts.svelte
+++ src/lib/components/home/contacts.svelte
@@ -5,17 +5,18 @@
 </script>
 
 {#snippet contactItem(contact: Contact)}
-	<a
-		data-aos="fade-up"
-		class="flex flex-row items-center gap-3 bg-green-200 px-6 py-4 border-4 border-green-700 shadow-md shadow-green-700 transition-all duration-200 hover:shadow-xl hover:shadow-black hover:bg-white hover:border-black"
-		href={contact.url}
-	>
-		<img src={contact.icon} class="size-8" alt={contact.label} />
-		<div class="text-black">
-			<div class="font-bold text-xl">{contact.label}</div>
-			<div class="text-sm text-stone-700">{contact.username}</div>
-		</div>
-	</a>
+	<div data-aos="zoom-in-up">
+		<a
+			class="flex flex-row items-center gap-3 bg-green-200 px-6 py-4 border-4 border-green-700 shadow-md shadow-green-700 transition-all duration-200 hover:shadow-xl hover:shadow-black hover:bg-white hover:border-black"
+			href={contact.url}
+		>
+			<img src={contact.icon} class="size-8" alt={contact.label} />
+			<div class="text-black">
+				<div class="font-bold text-xl">{contact.label}</div>
+				<div class="text-sm text-stone-700">{contact.username}</div>
+			</div>
+		</a>
+	</div>
 {/snippet}
 
 <section class="py-24 px-6">
