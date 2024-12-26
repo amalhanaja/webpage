31fb12db469d79380b1cf1627ebdd26bfb2f3888
amalhanaja <amalhanaja@gmail.com>
2024-12-26 23:32:00 +0700

    chore: init nextjs project

--- src/lib/helpers/date.ts
+++
@@ -1,6 +0,0 @@
-export const getLastYear = (): Date => {
-	const now = new Date();
-	const oneYear = 364 * 24 * 60 * 60 * 1000;
-	const lastYear = new Date(now.getTime() - oneYear);
-	return lastYear;
-};
