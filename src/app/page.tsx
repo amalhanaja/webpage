691b0a0e9a8fafebe1a9ee11fa1c7302f89df795
amalhanaja <amalhanaja@gmail.com>
2024-12-30 21:47:41 +0700

    feat: image profile block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import { Hero } from '@/components/sections/hero';
+import { Hero, HeroProps } from '@/components/sections/hero';
+
+const getWordings = (repeatCount: number) => {
+	const wordList = [
+		'SOFTWAREENGINEER',
+		'ANDROIDDEVELOPER',
+		'MOBILEDEVELOPER',
+		'BACKENDDEVELOPER',
+		'FRONTENDDEVELOPER',
+		'WEBDEVELOPER',
+		'TECHENTHUSIAST'
+	];
+	const results: string[] = [];
+	for (let i = 0; i < repeatCount; i++) {
+		results.push(...wordList);
+	}
+	return results;
+};
 
 export default function Home() {
+	const heroProps = {
+		name: 'Alfian Akmal Hanantio',
+		title: 'Software Engineer',
+		shortDescription: 'Software Engineer with a thirst for knowledge and a dedication to crafting elegant and efficient solutions. Constantly exploring new technologies and best practices.',
+		imageProfileWordList: getWordings(20).join("")
+	} satisfies HeroProps;
 	return (
 		<>
-			<Hero />
+			<Hero {...heroProps} />
 		</>
 	);
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
 			size: {
 				default: 'h-9 px-4 py-2 -translate-x-1 -translate-y-1 shadow hover:translate-x-0 hover:translate-y-0 hover:shadow-none',
 				sm: 'h-8 rounded-md px-3 text-xs',
-				lg: 'h-10 rounded-md text-lg px-8 -translate-x-1 -translate-y-1 shadow hover:translate-x-0 hover:translate-y-0 hover:shadow-none [&_svg]:size-6',
+				lg: 'h-12 rounded-md text-lg px-8 -translate-x-1 -translate-y-1 shadow hover:translate-x-0 hover:translate-y-0 hover:shadow-none [&_svg]:size-6',
 				icon: 'h-9 w-9 -translate-x-1 -translate-y-1 shadow hover:translate-x-0 hover:translate-y-0 hover:shadow-none'
 			}
 		},
