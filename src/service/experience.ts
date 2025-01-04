bfaafc740ccaa8a7d0c04a3e304558623847a9e1
amalhanaja <amalhanaja@gmail.com>
2025-01-04 15:54:08 +0700

    feat: fix toSorted not found

--- src/service/experience.ts
+++ src/service/experience.ts
@@ -4,23 +4,24 @@ import {ReactElement} from 'react';
 import {getContents} from '@/lib/mdx';
 
 export type Link = {
-	label: string;
-	href: string;
+    label: string;
+    href: string;
 }
 
 export type Experience = {
-	id: number;
-	title: string;
-	location: string;
-	logo: string,
-	links: Link[]
-	company: string;
-	workingRange: string;
-	content: ReactElement;
+    id: number;
+    title: string;
+    location: string;
+    logo: string,
+    links: Link[]
+    company: string;
+    workingRange: string;
+    content: ReactElement;
 }
 
 export const getExperiences = async (): Promise<Experience[]> => {
-	const experiences = await getContents<Experience>('experiences');
-	return experiences.map((e) => ({ ...e.frontmatter, content: e.content }))
-		.toSorted((a, b) => a.id - b.id);
+    const results = await getContents<Experience>('experiences');
+    const experiences = results.map((e) => ({...e.frontmatter, content: e.content}))
+    experiences.sort((a, b) => a.id - b.id);
+    return experiences
 };
--- src/service/project.ts
+++ src/service/project.ts
@@ -4,27 +4,28 @@ import path from 'path';
 import {compileMDX} from 'next-mdx-remote/rsc';
 
 export type Project = {
-	id: number;
-	title: string;
-	image: string;
-	tags: string[];
-	content: ReactElement;
-	github: string;
-	link: string;
+    id: number;
+    title: string;
+    image: string;
+    tags: string[];
+    content: ReactElement;
+    github: string;
+    link: string;
 }
 
 export const getProjects = async (): Promise<Project[]> => {
-	const files = await fs.readdir(path.join(process.cwd(), 'contents', 'projects'));
-	const results = await Promise.all(files.map(async file => {
-		const fileContent = await fs.readFile(path.join(process.cwd(), 'contents', 'projects', file), 'utf8');
-		const { content, frontmatter } = await compileMDX<Project>({
-			source: fileContent,
-			options: {
-				parseFrontmatter: true
-			}
-		});
-		return { content, frontmatter };
-	}));
-	return results.map((item) => ({ ...item.frontmatter, content: item.content }))
-		.toSorted((a, b) => a.id - b.id);
+    const files = await fs.readdir(path.join(process.cwd(), 'contents', 'projects'));
+    const results = await Promise.all(files.map(async file => {
+        const fileContent = await fs.readFile(path.join(process.cwd(), 'contents', 'projects', file), 'utf8');
+        const {content, frontmatter} = await compileMDX<Project>({
+            source: fileContent,
+            options: {
+                parseFrontmatter: true
+            }
+        });
+        return {content, frontmatter};
+    }));
+    const projects = results.map((item) => ({...item.frontmatter, content: item.content}))
+    projects.sort((a, b) => a.id - b.id);
+    return projects
 };
