891ccd4ed942f4371b21a93903a7e88c941d4ba4
amalhanaja <amalhanaja@gmail.com>
2025-01-04 16:34:26 +0700

    feat: force dynamic for blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,6 +2,8 @@ import {getPostList} from "@/service/blogs";
 import {PostLists} from "@/components/blocks/PostLists";
 import {HeroHighlight} from "@/components/ui/hero-highlight";
 
+export const dynamic = 'force-dynamic';
+
 export default async function BlogsPage() {
     const initialPosts = await getPostList({pageSize: 6, after: ""})
     return (
