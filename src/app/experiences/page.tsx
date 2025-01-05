f1565b02c70389f39a9e0733a039f752c2b16bbb
amalhanaja <amalhanaja@gmail.com>
2025-01-05 09:39:07 +0700

    feat: metadata for /experiences

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -1,9 +1,41 @@
-import { Experiences } from '@/components/blocks/experiences';
-import { getExperiences } from '@/service/experience';
+import {Experiences} from '@/components/blocks/experiences';
+import {getExperiences} from '@/service/experience';
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Experiences | Alfian Akmal Hanantio',
+    description: "Learn how I've contributed to successful projects throughout my career. Explore my professional journey and see how my skills and experience have helped me achieve impactful results.",
+    openGraph: {
+        title: 'Experiences | Alfian Akmal Hanantio',
+        description: "Learn how I've contributed to successful projects throughout my career. Explore my professional journey and see how my skills and experience have helped me achieve impactful results."
+    },
+    keywords: [
+        "projects",
+        "alfian",
+        "akmal",
+        "hanantio",
+        "kotlin",
+        "java",
+        "scalable",
+        "efficiency",
+        "key role",
+        "development",
+        "android",
+        "android developer",
+        "android engineer",
+        "software engineer",
+        "software",
+        "deliver",
+        "streamline",
+        "application",
+        "leadership",
+        "team"
+    ]
+}
 
 export default async function ExperiencesPage() {
-	const experiences = await getExperiences();
-	return (
-		<Experiences experiences={experiences} />
-	);
+    const experiences = await getExperiences();
+    return (
+        <Experiences experiences={experiences}/>
+    );
 };
