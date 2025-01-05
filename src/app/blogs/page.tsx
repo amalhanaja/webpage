09c639ebf56b5b9feb8188e21a887431b10de769
amalhanaja <amalhanaja@gmail.com>
2025-01-05 09:53:54 +0700

    feat: metadata for /blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,18 @@
 import {getPostList} from "@/service/blogs";
 import {PostLists} from "@/components/blocks/PostLists";
 import {HeroHighlight} from "@/components/ui/hero-highlight";
+import {Metadata} from "next";
 
 export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+    title: "Blog - Alfian Akmal Hanantio",
+    description: "Check out my blog where I share my learnings and perspectives on the tech world, including web, mobile, AI and emerging technologies. I explore trends, share best practices, and offer insights into the ever-changing tech landscape.",
+    openGraph: {
+        title: "Blog - Alfian Akmal Hanantio",
+        description: "Check out my blog where I share my learnings and perspectives on the tech world, including web, mobile, AI and emerging technologies. I explore trends, share best practices, and offer insights into the ever-changing tech landscape."
+    }
+}
 
 export default async function BlogsPage() {
     const initialPosts = await getPostList({pageSize: 6, after: ""})
