3af283d410d793749395cc608c95687842460e3c
amalhanaja <amalhanaja@gmail.com>
2025-01-06 15:55:47 +0700

    feat: experiences page animation

--- src/components/blocks/experiences.tsx
+++ src/components/blocks/experiences.tsx
@@ -11,6 +11,7 @@ import {cn} from '@/lib/utils';
 import {ArrowLeftToBottom} from '@/components/ui/arrow-left-to-bottom';
 import {Experience} from '@/service/experience';
 import {HeroHighlight} from '@/components/ui/hero-highlight';
+import {motion} from 'motion/react';
 
 type ExperiencesProps = {
     experiences: Experience[]
@@ -19,10 +20,19 @@ type ExperiencesProps = {
 export const ExperienceItem = ({experience, index, isLast}: {
     experience: Experience,
     index: number,
-    isLast: boolean
+    isLast: boolean,
 }) => {
+    const oddVariants = {
+        initial: {opacity: 0, transform: 'translateX(-50px)'},
+        show: {opacity: 1, transform: 'translateX(0)'},
+    }
+    const evenVariants = {
+        initial: {opacity: 0, transform: 'translateX(50px)'},
+        show: {opacity: 1, transform: 'translateX(0)'},
+    }
     return (
-        <div className="flex flex-row items-center relative">
+        <motion.div className="flex flex-row items-center relative"
+                    variants={index % 2 == 0 ? oddVariants : evenVariants}>
             {index % 2 === 1 && !isLast && (<div className="hidden xl:block"><ArrowLeftToBottom
                 className="absolute stroke-stone-600 dark:stroke-stone-300 scale-y-[400%] scale-x-[400%] translate-x-[250%] left-0 bottom-12"/>
             </div>)}
@@ -32,7 +42,8 @@ export const ExperienceItem = ({experience, index, isLast}: {
                     <div
                         className="h-16 aspect-square shadow border-2 rounded-full flex items-center justify-center bg-card">
                         {experience.logo ? (
-                            <Image src={experience.logo} alt={experience.company} title={experience.company} width={24} height={24}
+                            <Image src={experience.logo} alt={experience.company} title={experience.company} width={24}
+                                   height={24}
                                    className="size-8 object-center"/>) : (
                             <Building2 size={32} aria-label={experience.company}/>)}
                     </div>
@@ -67,29 +78,66 @@ export const ExperienceItem = ({experience, index, isLast}: {
             {index % 2 == 0 && !isLast && (<div className="hidden xl:block"><ArrowLeftToBottom
                 className="absolute stroke-stone-600 dark:stroke-stone-300 scale-y-[400%] -scale-x-[400%] -translate-x-[250%] right-0 bottom-12"/>
             </div>)}
-        </div>
+        </motion.div>
     );
 };
 
 export const Experiences = ({experiences}: ExperiencesProps) => {
+    const sectionTitleVariants = {
+        initial: {opacity: 0, transform: 'translateX(-60px)'},
+        show: {opacity: 1, transform: 'translateX(0)'},
+    }
+    const sectionSubtitleVariants = {
+        initial: {opacity: 0, transform: 'translateX(-60px)'},
+        show: {opacity: 1, transform: 'translateX(0)', transition: {delay: 0.1}},
+    }
+    const experiencesContainerVariants = {
+        initial: {opacity: 0, transform: 'translateY(60px)'},
+        show: {
+            opacity: 1,
+            transform: 'translateY(0)',
+            transition: {delay: 0.2, ease: 'easeIn', staggerChildren: 0.2, delayChildren: 0.4},
+        },
+    }
     return (
         <HeroHighlight className="w-full max-w-screen-xl mx-auto px-4 py-32 flex flex-col">
-            <SectionTitle className="uppercase bg-card">
+            <SectionTitle className="uppercase bg-card" variants={sectionTitleVariants} initial="initial"
+                          animate="show">
                 <Briefcase/>
                 <h1>Work Experiences</h1>
             </SectionTitle>
-            <h2 className="text-4xl tracking-wider font-bold mt-6">A summary of my professional experience.</h2>
-            <div className="flex flex-col gap-8 mt-16 w-full">
+            <motion.h2
+                className="text-4xl tracking-wider font-bold mt-6"
+                variants={sectionSubtitleVariants}
+                initial="initial"
+                animate="show"
+            >
+                A summary of my professional experience.
+            </motion.h2>
+            <motion.div
+                className="flex flex-col gap-8 mt-16 w-full"
+                variants={experiencesContainerVariants}
+                initial="initial"
+                animate="show"
+            >
                 {experiences.map((experience, index) => {
                     return (
                         <ExperienceItem
                             key={index}
                             index={index}
                             experience={experience}
-                            isLast={experiences.length - 1 == index}/>);
+                            isLast={experiences.length - 1 == index}
+                        />
+                    );
                 })}
-            </div>
-            <div className="mt-8 flex flex-col items-center">
+            </motion.div>
+            <motion.div className="mt-8 flex flex-col items-center"
+                        variants={{
+                            ...sectionSubtitleVariants,
+                            show: {...sectionTitleVariants.show, transition: {delay: 1.4}}
+                        }}
+                        initial="initial"
+                        animate="show">
                 <Button className="uppercase w-fit" variant="primary" size="lg" asChild>
                     <Link href="/alfian-resume.pdf" target="_blank" title="resume">
                         <span>View Full Resume</span>
@@ -97,7 +145,7 @@ export const Experiences = ({experiences}: ExperiencesProps) => {
                     </Link>
 
                 </Button>
-            </div>
+            </motion.div>
         </HeroHighlight>
     );
 };
