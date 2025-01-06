8d9cc8ac94a17eb06bf04f4dac3c48f9b28de531
amalhanaja <amalhanaja@gmail.com>
2025-01-06 15:36:37 +0700

    feat: experience summary animation

--- src/components/blocks/experience-summary.tsx
+++ src/components/blocks/experience-summary.tsx
@@ -4,21 +4,56 @@ import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card';
 import {Experience} from '@/service/experience';
 import Image from 'next/image';
 import {Building2} from 'lucide-react';
+import {motion} from 'motion/react';
 
 type Props = {
     experiences: Experience[];
 }
 
 export const ExperienceSummary = ({experiences}: Props) => {
+    const experiencesContainerVariants = {
+        initial: {opacity: 0, transform: 'translateY(60px)'},
+        show: {
+            opacity: 1,
+            transform: 'translateY(0)',
+            transition: {duration: 0.2, ease: 'easeIn', delay: 1.3, staggerChildren: 0.2, delayChildren: 1}
+        },
+    }
+    const experienceAnimationVariants = {
+        initial: {opacity: 0, transform: 'translateY(60px)'},
+        show: {opacity: 1, transform: 'translateY(0)'},
+    }
     return (
-        <Card className="shadow-none col-span-12 sm:col-span-6">
+        <Card
+            className="shadow-none col-span-12 sm:col-span-6"
+            initial={{opacity: 0, scale: 0}}
+            animate={{opacity: 1, scale: 1}}
+            transition={{type: 'spring', stiffness: 100, damping: 15, delay: 1}}
+        >
             <CardHeader>
-                <CardTitle><h2 className="text-3xl font-bold uppercase">Experience</h2></CardTitle>
+                <CardTitle>
+                    <motion.h2
+                        className="text-3xl font-bold uppercase"
+                        initial={{opacity: 0, transform: 'translateY(60px)'}}
+                        animate={{opacity: 1, transform: 'translateY(0)'}}
+                        transition={{duration: 0.2, ease: 'easeIn', delay: 1.2}}
+                    >
+                        Experience
+                    </motion.h2>
+                </CardTitle>
             </CardHeader>
-            <CardContent className="flex flex-col gap-2">
+            <CardContent
+                className="flex flex-col gap-2"
+                variants={experiencesContainerVariants}
+                initial="initial"
+                animate="show"
+            >
                 {experiences.map((experience: Experience, index: number) => {
                     return (
-                        <Card key={index} className="shadow-sm bg-netral">
+                        <Card
+                            key={index} className="shadow-sm bg-netral"
+                            variants={experienceAnimationVariants}
+                        >
                             <div className="flex flex-row gap-6 p-4">
                                 <div
                                     className="h-12 aspect-square shadow-sm border-2 rounded-full flex items-center justify-center bg-card">
