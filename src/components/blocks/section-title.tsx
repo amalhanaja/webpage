427273862c7f0f5fe63ebf5a4caadd778d62ccec
amalhanaja <amalhanaja@gmail.com>
2025-01-06 15:55:47 +0700

    feat: experiences page animation

--- src/components/blocks/section-title.tsx
+++ src/components/blocks/section-title.tsx
@@ -1,22 +1,23 @@
 'use client';
 
-import { cn } from '@/lib/utils';
+import {cn} from '@/lib/utils';
 import * as React from 'react';
+import {HTMLMotionProps, motion} from "motion/react";
 
 const SectionTitle = React.forwardRef<
-	HTMLParagraphElement,
-	React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-	<div
-		ref={ref}
-		className={cn(
-			'flex flex-row gap-4 cursor-pointer w-fit items-center',
-			'text-lg font-semibold uppercase border-2 py-2 px-4 bg-background text-foreground tracking-wide',
-			className
-		)}
-		{...props}
-	/>
+    HTMLParagraphElement,
+    HTMLMotionProps<"div">
+>(({className, ...props}, ref) => (
+    <motion.div
+        ref={ref}
+        className={cn(
+            'flex flex-row gap-4 cursor-pointer w-fit items-center',
+            'text-lg font-semibold uppercase border-2 py-2 px-4 bg-background text-foreground tracking-wide',
+            className
+        )}
+        {...props}
+    />
 ));
 SectionTitle.displayName = 'CardTitle';
 
-export { SectionTitle };
+export {SectionTitle};
